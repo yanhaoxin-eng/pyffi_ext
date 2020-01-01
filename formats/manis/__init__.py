eb1ca87190835b23ffb6a100076426656df4c0d2
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-01-01 23:54:08 +0100

    More manis stuff

--- formats/manis/__init__.py
+++ formats/manis/__init__.py
@@ -152,10 +152,27 @@ class ManisFormat(pyffi.object_models.xml.FileFormat):
 			print("name_indices_1", name_indices_1)
 			name_indices_2 = struct.unpack(f"<{mani_info.e}I", stream.read(mani_info.e * 4))
 			print("name_indices_2", name_indices_2)
-			p_indices_0 = struct.unpack(f"<{mani_info.c*2}B", stream.read(mani_info.c * 2))
+			p_indices_0 = struct.unpack(f"<{mani_info.c}B", stream.read(mani_info.c))
 			print("p_indices_0", p_indices_0)
-			p_indices_1 = struct.unpack(f"<{mani_info.name_count*2}B", stream.read(mani_info.name_count * 2))
+			p_indices_1 = struct.unpack(f"<{mani_info.name_count}B", stream.read(mani_info.name_count))
 			print("p_indices_1", p_indices_1)
+			p_indices_2 = struct.unpack(f"<{mani_info.e}B", stream.read(mani_info.e))
+			print("p_indices_2", p_indices_2)
+
+			# seems to be pretty good until here, then it breaks
+
+			# flags per frame?
+			frames_0 = [ struct.unpack(f"<4B", stream.read(4)) for i in range(mani_info.frame_count)]
+			print("frames_0", frames_0)
+			print("pad", stream.read(1))
+			# could be bitfields per frame?
+			frames_1 = struct.unpack(f"<{mani_info.frame_count}I", stream.read(mani_info.frame_count * 4))
+			print("frames_1", frames_1)
+			zerom, frame_count, name_count, c_count = struct.unpack(f"<4I", stream.read(4 * 4))
+			print("zerom, frame_count, name_count, c_count",zerom, frame_count, name_count, c_count)
+			print("zeros", stream.read(76))
+			x, y = struct.unpack(f"<2H", stream.read(4))
+			print("x, y", x, y)
 
 		def write(self, stream, verbose=0, file=""):
 			"""Write a dds file.
