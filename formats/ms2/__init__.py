83312b29edb404b57ee51234d705b83407508ff2
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-01-25 23:04:35 +0100

    Pretty print 3x3 matrices

--- formats/ms2/__init__.py
+++ formats/ms2/__init__.py
@@ -78,14 +78,22 @@ class Ms2Format(pyffi.object_models.xml.FileFormat):
 	float = pyffi.object_models.common.Float
 	SizedString = pyffi.object_models.common.SizedString
 	ZString = pyffi.object_models.common.ZString
-	
+
+	class Matrix33:
+		def __str__(self):
+			return (
+					"[ %6.3f %6.3f %6.3f ]\n"
+					"[ %6.3f %6.3f %6.3f ]\n"
+					"[ %6.3f %6.3f %6.3f ]\n"
+					% (self.m_11, self.m_12, self.m_13, self.m_21, self.m_22, self.m_23, self.m_31, self.m_32, self.m_33))
+
 	class Matrix44:
 		def __str__(self):
 			return f"{self.__class__} instance at {id(self):02x}\n" \
-					f"\t[{self.m_11:7.3f}, {self.m_12:7.3f}, {self.m_13:7.3f}, {self.m_14:7.3f}]\n" \
-					f"\t[{self.m_21:7.3f}, {self.m_22:7.3f}, {self.m_23:7.3f}, {self.m_24:7.3f}]\n" \
-					f"\t[{self.m_31:7.3f}, {self.m_32:7.3f}, {self.m_33:7.3f}, {self.m_34:7.3f}]\n" \
-					f"\t[{self.m_41:7.3f}, {self.m_42:7.3f}, {self.m_43:7.3f}, {self.m_44:7.3f}]"
+					f"\t[{self.m_11:7.3f} {self.m_12:7.3f} {self.m_13:7.3f} {self.m_14:7.3f}]\n" \
+					f"\t[{self.m_21:7.3f} {self.m_22:7.3f} {self.m_23:7.3f} {self.m_24:7.3f}]\n" \
+					f"\t[{self.m_31:7.3f} {self.m_32:7.3f} {self.m_33:7.3f} {self.m_34:7.3f}]\n" \
+					f"\t[{self.m_41:7.3f} {self.m_42:7.3f} {self.m_43:7.3f} {self.m_44:7.3f}]"
 
 		def as_list(self):
 			"""Return matrix as 4x4 list."""
