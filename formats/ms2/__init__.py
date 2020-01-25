923a18205016615c6e1c256df13e2bbb67f14761
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-01-25 22:59:01 +0100

    Pretty print 4x4 matrices

--- formats/ms2/__init__.py
+++ formats/ms2/__init__.py
@@ -80,6 +80,13 @@ class Ms2Format(pyffi.object_models.xml.FileFormat):
 	ZString = pyffi.object_models.common.ZString
 	
 	class Matrix44:
+		def __str__(self):
+			return f"{self.__class__} instance at {id(self):02x}\n" \
+					f"\t[{self.m_11:7.3f}, {self.m_12:7.3f}, {self.m_13:7.3f}, {self.m_14:7.3f}]\n" \
+					f"\t[{self.m_21:7.3f}, {self.m_22:7.3f}, {self.m_23:7.3f}, {self.m_24:7.3f}]\n" \
+					f"\t[{self.m_31:7.3f}, {self.m_32:7.3f}, {self.m_33:7.3f}, {self.m_34:7.3f}]\n" \
+					f"\t[{self.m_41:7.3f}, {self.m_42:7.3f}, {self.m_43:7.3f}, {self.m_44:7.3f}]"
+
 		def as_list(self):
 			"""Return matrix as 4x4 list."""
 			return [
